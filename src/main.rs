dc926aeaf30a063ff1ba7d1d6e47c9e8959af977
Denisse <denissevalle16@gmail.com>
2025-09-29 23:11:20 -0600

    Segundo commit: Proyecto inicial del monitor de memoria

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,16 @@ use systemstat::{System, Platform};
 
 fn main() {
     let sys = System::new();
+
     match sys.memory() {
         Ok(mem) => {
-            //  println!("Total: {} MiB", mem.total);
-            // println!("Used: {} MiB", mem.used);
-           
-	    println!("Free: {} MiB", mem.free);
-        }
-        Err(e) => eprintln!("Error getting memory info: {}", e),
+            // systemstat devuelve los valores en Kilobytes, hay que convertir.
+            let total_gb = mem.total.as_u64() as f64 / 1024.0 / 1024.0;
+            let free_gb = mem.free.as_u64() as f64 / 1024.0 / 1024.0;
+
+            println!("Memoria Total: {:.2} GB", total_gb);
+            println!("Memoria Libre: {:.2} GB", free_gb);
+        },
+        Err(e) => eprintln!("Error al obtener info de la memoria: {}", e),
     }
 }
